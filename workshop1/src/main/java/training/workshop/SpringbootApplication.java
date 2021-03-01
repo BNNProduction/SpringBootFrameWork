f41330ead8735ab9ca09ae0cd67b47e94edd41f9
uthai <uthai.b@kbtg.tech>
2021-03-01 11:11:16 +0700

    init

---
+++ workshop1/src/main/java/training/workshop/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package training.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
